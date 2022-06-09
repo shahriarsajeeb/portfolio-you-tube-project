c7bcb057764e379c26462cc3c69ab9ecec500e74
umeshyadavrocks <umeshyadavrocks@gmail.com>
2022-06-09 23:15:31 +0530

    lets start

--- src/Home.jsx
+++ src/Home.jsx
@@ -40,11 +40,11 @@ function Home() {
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-justify white pointer"
+              className="bi bi-justify white pointer"
               viewBox="0 0 16 16"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
               />
             </svg>
